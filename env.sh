b3fbf2b4a7ca3ec84df6e5cb3508a9aa901f7130
yupenglei <yupenglei@kuaishou.com>
2025-04-06 00:57:47 +0800

    补充langsmith参数

---
+++ env.sh
@@ -0,0 +1,6 @@
+# langsmith参数
+export LANGSMITH_TRACING=true
+export LANGSMITH_ENDPOINT="https://api.smith.langchain.com"
+#export LANGSMITH_API_KEY="<your-api-key>"
+export LANGSMITH_PROJECT="MyAIServer"
+#export OPENAI_API_KEY="<your-openai-api-key>"
